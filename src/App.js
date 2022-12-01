f8d7f4a377fcc94b269cd9618b3bde3989cb44be
KiraaCorsac <wire150@gmail.com>
2022-12-01 14:04:48 +0100

    cookie demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Cookies from 'universal-cookie';
+import { useState } from "react";
+
 
 function App() {
+  const cookies = new Cookies();
+
+  if (cookies.get("buttonPresses") == undefined) {
+    cookies.set("buttonPresses", 0);
+  }
+
+
+  const [buttonPresses, setButtonPresses] = useState(cookies.get("buttonPresses"))
+
+
+  // cookies.set("buttonPresses", 1);
+  console.log("non-existing cookie: ", cookies.get("nonexistingCookie"));
+
+  function onButtonClick() {
+    console.log("Button clicked!");
+    console.log(cookies.get("buttonPresses"));
+
+    const presses = Number(cookies.get("buttonPresses")) + 1;
+    cookies.set("buttonPresses", presses)
+    setButtonPresses(presses)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onButtonClick}>Press me!</button>
+
+
+      Button presses: {buttonPresses}.
     </div>
   );
 }
